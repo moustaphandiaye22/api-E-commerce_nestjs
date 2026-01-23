8d463eef89c73928ccbb5f403d7d7b1c86bbd1ae
Moustapha Ndiaye <moustapha.ndiaye@example.com>
2026-01-23 22:58:46 +0000

    feat: add AuditService to AddressesModule and AuthModule providers

--- src/modules/addresses/addresses.module.ts
+++ src/modules/addresses/addresses.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AddressesController } from './addresses.controller';
 import { AddressesService } from './addresses.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AuditService } from '../../common/services/audit.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AddressesController],
-  providers: [AddressesService],
+  providers: [AddressesService, AuditService],
   exports: [AddressesService],
 })
 export class AddressesModule {}
